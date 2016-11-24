3e4e6f9c68c6fe4ffc61cd93f96bb9d890f1fd0c
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-11-24 10:49:33 +0100

    Update dadi.parallel.sh

--- dadi.parallel.sh
+++ dadi.parallel.sh
@@ -4,7 +4,7 @@ for i in `seq 1 1 25`; do
 
 # test models demo dadi #
 
-python2.6 script_inference_anneal3_newton.py -f ../Data/scandinavia_LD_project40_spectrum_from_dadi.v6.fs -y South -x West -p 50,60,70 -m SI,IM,AM,SC,DB -l -v
+python2.6 dadi.run.py -f ../Data/scandinavia_LD_project40_spectrum_from_dadi.v6.fs -y South -x West -p 50,60,70 -m SI,IM,AM,SC,DB -l -v
 
 ((i++))
 done
